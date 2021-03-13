2589a1a688cb4f292cb78cb2aa4ac376b0834901
Tiago de Abreu <devtiagoabreu@gmail.com>
2021-03-13 17:28:16 -0300

    tocando áudio no início do desafio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -46,13 +46,13 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     setActiveChallenge(challenge);
 
+    new Audio('/notification.mp3').play;
+
     if (Notification.permission === 'granted') {
       new Notification('Novo desafio :chess_pawn:', {
         body: `Valendo ${challenge.amount}xp!`
       })
-      
     }
-
   }
 
   function resetChallenge() {
